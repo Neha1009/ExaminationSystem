37636ef61f25844bd5db1580340ec458e362694b
Neha S Shetty <nehashetty1009@gmail.com>
2022-08-17 14:23:02 +0530

    Update project.sql

--- SQL Files/project.sql
+++ SQL Files/project.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Nov 20, 2019 at 04:29 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
